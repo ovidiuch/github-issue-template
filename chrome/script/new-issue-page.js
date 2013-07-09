cfa73741377a74250c20d31392e6d5c787655066
Ovidiu Chereches <skidding@gmail.com>
2013-07-09 11:56:45 +0700

    Load issue template in issue body on load #2

--- chrome/script/new-issue-page.js
+++ chrome/script/new-issue-page.js
@@ -1,3 +1,20 @@
+var TEMPLATE_PATH = '//raw.github.com/skidding/github-issue-template/master/README.md';
+var $ISSUE_BODY = $('.composer [name="issue[body]"]');
+
 $(function() {
-  console.log('Github new issues page');
+  // Let the user now immediately that a template is being fetched for the
+  // fresh issue they're about to create
+  $ISSUE_BODY.prop('placeholder', "Loading issue template...");
+  // Fetch template contents from a Github project repo directly (this is
+  // conventient because, being on the same domain (github.com), AJAX requests
+  // are possible)
+  $.get(TEMPLATE_PATH, function(contents, status) {
+    if (status == 'success') {
+      $ISSUE_BODY.val($.trim(contents));
+    } else {
+      // Notify the user that the template couldn't be fetched and the they
+      // should carry on w/ a blank issue body
+      $ISSUE_BODY.prop('placeholder', "Couldn't fetch issue template. Sorry!");
+    }
+  });
 });
