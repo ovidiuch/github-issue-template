6c99d32e1154000fe617d6e18ff528af5368c46c
Ovidiu Chereches <skidding@gmail.com>
2013-07-09 17:32:49 +0700

    Fix default list of repos in popup #5

--- chrome/script/popup.js
+++ chrome/script/popup.js
@@ -1,7 +1,14 @@
+var STORAGE_KEY = 'github-repos';
+
 $(function() {
+  // Default the list of repos to the uberVU/mozaic repo if nothing has been
+  // submitted yet
+  if (localStorage[STORAGE_KEY] === undefined) {
+    localStorage[STORAGE_KEY] = 'uberVU/mozaic';
+  }
   // Create a list of clickable repos, loaded from local storage (saved from
   // the options page)
-  var githubRepos = $.trim(localStorage['github-repos']).split("\n");
+  var githubRepos = $.trim(localStorage[STORAGE_KEY]).split("\n");
   for (var i = 0, path, user, repo, href; i < githubRepos.length; i++) {
     path = githubRepos[i].split('/');
     user = path[0];
